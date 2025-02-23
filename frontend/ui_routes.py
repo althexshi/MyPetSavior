6aed332b422a09722bd15318fd5f1fe969f86e60
Dmitriy Gamolya <109139706+DrowsyTM@users.noreply.github.com>
2025-02-23 02:19:03 -0800

    Reverted non-functioning ui changes

--- frontend/ui_routes.py
+++ frontend/ui_routes.py
@@ -6,18 +6,20 @@ from nicegui import ui
 def add_ui_routes():
     @ui.page("/")
     def main_page():
-        ui.add_head_html('''
-            <style>
-                .search-container { position: relative; width: 650px; }
-                .search-container input { width: 100%; padding-left: 50px; }
-                .search-icon { position: absolute; left: 15px; top: 50%; transform: translateY(-50%); font-size: 24px; color: #008080; }
-            </style>
-        ''')
+        # ui.add_head_html('''
+        #     <style>
+        #         .search-container { position: relative; width: 650px; }
+        #         .search-container input { width: 100%; padding-left: 50px; }
+        #         .search-icon { position: absolute; left: 15px; top: 50%; transform: translateY(-50%); font-size: 24px; color: #008080; }
+        #     </style>
+        # ''')
 
         ui.add_head_html('<link href="https://cdn.jsdelivr.net/themify-icons/0.1.2/css/themify-icons.css" rel="stylesheet" />')
-        ui.label("Welcome to Pet Savior!").classes("w-full text-center").style("font-size: 100px; font-weight: bold;")
-        with ui.element('div').classes('search-container'):
-            ui.html('<i class="ti-search search-icon"></i>')
+        ui.label("Welcome to PetSavior!").classes("w-full text-center").style("font-size: 100px; font-weight: bold;")
+        # with ui.element('div').classes('search-container'):
+        with ui.row().classes("justify-center items-center w-full text-center"):
+            # ui.html('<i class="ti-search search-icon"></i>')
+            ui.icon('search-icon').classes('text-5xl').style('margin-right: 10px')
             search_input = ui.input(placeholder='Search for Pets...')
             search_input.style(
                 'border-radius: 25px; '
@@ -26,10 +28,5 @@ def add_ui_routes():
                 'font-size: 16px; '
                 'width: 650px;'
         )
-
-    # @ui.page("/search?{text}")
-    # def search_page(text):
-
-
 
 
