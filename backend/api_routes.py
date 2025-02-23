4d781cd52d5e8c4ee8eb7a576b46f995d2a1dbed
Dima <dima.gamolya@gmail.com>
2025-02-23 10:18:54 -0800

    Critical bugfix

--- backend/api_routes.py
+++ backend/api_routes.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database.database import engine  # Base should be the declarative base from SQLAlchemy
 from database.database import Base
-from database.models import PetDetails  # Ensure that PetDetails is defined in models.py
+from database.models import Animals  # Ensure that PetDetails is defined in models.py
 from database.database import get_db
 
 def add_api_routes(app: FastAPI):
@@ -16,13 +16,13 @@ def add_api_routes(app: FastAPI):
 
     @app.get("/pets/{pet_id}")
     def get_pet(pet_id: int, db: Session = Depends(get_db)):
-        pet = db.query(PetDetails).filter(PetDetails.pet_id == pet_id).first()
+        pet = db.query(Animals).filter(Animals.pet_id == pet_id).first()
         if pet is None:
             raise HTTPException(status_code=404, detail="Pet not found")
         return pet
 
     @app.post("/pets/")
-    def create_pet(pet: PetDetails, db: Session = Depends(get_db)):
+    def create_pet(pet: Animals, db: Session = Depends(get_db)):
         db.add(pet)
         db.commit()
         db.refresh(pet)
