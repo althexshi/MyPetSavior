bdb371cacdefa5eba593a85e3031609855ea3ccc
brand <brandonhu329@gmail.com>
2025-02-22 18:16:32 -0800

    Not working, trying to center text

--- frontend/ui_routes.py
+++ frontend/ui_routes.py
@@ -7,8 +7,9 @@ def add_ui_routes():
     @ui.page("/")
     def main_page():
         ui.add_head_html('<link href="https://cdn.jsdelivr.net/themify-icons/0.1.2/css/themify-icons.css" rel="stylesheet" />')
-        ui.label("Welcome to PetSavior!").style("font-size: 100px; font-weight: bold; text-align: center;")
-        with ui.row().classes("justify-center"):
+        with ui.row().classes('justify-center'):
+            ui.label("Welcome to PetSavior!").style("font-size: 100px; font-weight: bold;")
+        with ui.row().classes('justify-center'):
             ui.icon('ti-heart').classes('text-5xl').style('margin-right: 10px;')
             search_input = ui.input(placeholder='Search For Pets...')
             search_input.style(
