f127c1a3a63ff2977f860185244b4f879017924c
Dima <dima.gamolya@gmail.com>
2025-02-23 10:01:46 -0800

    in progress commit

--- backend/api_routes.py
+++ backend/api_routes.py
@@ -4,19 +4,19 @@ from sqlalchemy.orm import Session
 from database.database import SessionLocal, engine, Base, get_db
 from database.models import Animals
 
-def add_api_routes(app: FastAPI):
-    @app.get("/api/search")
-    def search_pets(query: str = None):
+app = FastAPI()
 
-        print("Begin query")
-        db: Session = SessionLocal()
-        try:
-            result = db.query(Animals.pet_name).all()
-            pets = [row[0] for row in result]
-            return pets
-        except Exception as e:
-            print(f"Error reading {e}")
-            return None
-        finally:
-            db.close()
+@app.get("/api/search")
+def search_pets(query: str = None):
+    print("Begin query")
+    db: Session = SessionLocal()
+    try:
+        result = db.query(Animals.pet_name).all()
+        pets = [row[0] for row in result]
+        return pets
+    except Exception as e:
+        print(f"Error reading {e}")
+        return None
+    finally:
+        db.close()
 
