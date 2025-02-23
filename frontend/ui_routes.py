b2497e54030055839f0985935b1b3c0f3750c55d
brand <brandonhu329@gmail.com>
2025-02-22 17:05:32 -0800

    Added Name

--- frontend/ui_routes.py
+++ frontend/ui_routes.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from nicegui import ui
 
-def add_ui_routes():
-    @ui.page("/")
-    def main_page():
-        ui.label("Welcome to PetSavior!").style("font-size: 24px; font-weight: bold;")
-        ui.button("Find a Pet", on_click=lambda: ui.notify("Coming soon!"))
+result = ui.label('')
+
+@ui.page("/")
+def main_page():
+    ui.label("Welcome to PetSavior!").style("font-size: 24px; font-weight: bold;")
+    ui.button("Find a Pet", on_click=lambda: ui.notify("Coming soon!"))
+    ui.input(label='Text', placeholder='start typing', on_change=lambda e: result.set_text('you typed: ' + e.value),
+             validation={'Input too long': lambda value: len(value) < 20})
+
+ui.run()
+
